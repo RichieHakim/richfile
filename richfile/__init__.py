17951a57002b959ea6d15bf793df47892a574ade
RichieHakim <RichHakim@gmail.com>
2024-09-15 15:24:06 -0400

    version added

--- richfile/__init__.py
+++ richfile/__init__.py
@@ -1,3 +1,5 @@
 ## Import important stuff from util.py into top-level namespace
 import util
 from util import RichFile, load_folder, load_element, load_metadata, save_object
+
+__version__ = "0.1.0"
